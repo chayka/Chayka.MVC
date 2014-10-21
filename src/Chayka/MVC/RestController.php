359a9b85d7f2849e5c9fe3f23b52def654dc99f7
Boris Mossounov <borix@tut.by>
2014-10-21 19:54:43 +0300

    Moving to github repository

---
+++ src/Chayka/MVC/RestController.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: borismossounov
+ * Date: 21.10.14
+ * Time: 19:53
+ */
+
+namespace Chayka\MVC;
+
+
+class RestController extends Controller {
+// TODO: Implement rest controller
+} 
